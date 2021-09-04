ad28d96a380148b1c76e3a30e075c4fb5c3c2e56
Arnaldo Aparicio <apaarn2211@gmail.com>
2021-09-03 21:03:07 -0400

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,14 @@
+
+movie = "Titanic"
+is_entertaining = false
+
+movie = "Jurassic Park"
+
+num_4 = 4
+num_6 = 6
+product = num_4 * num_6
+
+favorite_foods = ["pizza", "cheeseburger", "shrimp taco"]
+favorite_foods.shift
+
+puts movie.length
