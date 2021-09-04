ad9c853b69061d1add058c48bbc13b60f7f0a08d
Arnaldo Aparicio <apaarn2211@gmail.com>
2021-09-03 21:28:07 -0400

    Add developer hash model

---
+++ models/model_b.rb
@@ -0,0 +1,11 @@
+developer = {
+"name" => 'David',
+"alma_mater" => 'Turing School of Software and Design', 
+"years_of_experience" => 4,
+"dev_debut" => 2017,
+"is_employed" => true,
+"worked_remote" => false,
+"languages_learned" => ['Ruby', 'Javascript', 'Java'],
+"past_employers" => ['Uber', 'Google', 'Turing'],
+
+}
