01d6da7f968a5fe6fc7aa53357ae3200f0ae6088
Arnaldo Aparicio <apaarn2211@gmail.com>
2021-09-03 21:20:24 -0400

    Add dinosaur hash model

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+dinosaur = {
+"type" => 'Brachiosaurus', 
+"era" => 'Late Jurrasic',
+"height_in_feet" => 41,
+"is_herbivorous" => true,
+"total_teeth" => 52,
+"stands_upright" => false,
+"body_features" => ['Very long neck', 'Small head'],
+"close_relatives" => ['Brontosaurus', 'Apatosaurus', 'Diplodocus']
+}
